81e4f5f089414c02446923b5e64d6e05c3fdb925
Farzad Majidfayyaz <farzadmf@hotmail.com>
2018-08-26 19:03:04 -0400

    Hide products if user is not logged in

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,18 +1,22 @@
 <template lang="pug">
-  v-container
+  v-container()
     v-layout(row pa-2)
       h1 Product List
-    app-product-list
+    app-product-list(v-if="isLoggedIn")
+    v-layout(pa-2 v-else)
+      p Please login to be able to purchase products
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import { mapGetters } from 'vuex';
 import AppProductList from '@/components/ProductList.vue';
 
 @Component({
   components: {
     AppProductList,
   },
+  computed: mapGetters(['isLoggedIn']),
 })
 export default class AppProducts extends Vue {}
 </script>
