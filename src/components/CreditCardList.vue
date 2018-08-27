f688fdaa4461134f1cef92e8aaa6dedcf9940f13
Farzad Majidfayyaz <farzadmf@hotmail.com>
2018-08-27 14:49:36 -0400

    Add my card images and card list component

---
+++ src/components/CreditCardList.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+  v-flex(xs12 text-xs-center)
+    v-carousel(:cycle="false" dark hide-delimiters)
+      v-carousel-item
+        div Hello1
+      v-carousel-item
+        div Hello2
+      v-carousel-item
+        div Hello3
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class AppCreditCardList extends Vue {}
+</script>
+
+<style scoped lang="scss">
+$width: 500px;
+
+.v-carousel {
+  height: $width / 1.67;
+  width: $width;
+  margin: 0 auto;
+}
+</style>
--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -1,12 +1,17 @@
 <template lang="pug">
-  v-layout(fill-height column)
-    v-flex(xs2)
+  v-layout(row wrap)
+    v-flex(xs12)
       h1 Your Shopping Cart
-    v-flex(xs4)
+
+    v-flex(xs12)
       p.
         You have selected {{ selectedProducts.length }} products to purchase. Click
         the 'Add' button below to add a new credit cart.
-    v-flex(xs2 text-xs-center)
+
+    v-flex(xs12)
+      app-credit-card-list
+
+    v-flex(xs12 text-xs-center)
       v-btn(large color="primary" @click.stop="dialog = true") Add Credit Card
 
     v-dialog(v-model="dialog" full-width)
@@ -18,10 +23,12 @@ import { Component, Vue } from 'vue-property-decorator';
 import { mapGetters } from 'vuex';
 import { Getters } from '@/store';
 import AppCreditCardForm from '@/components/CreditCardForm.vue';
+import AppCreditCardList from '@/components/CreditCardList.vue';
 
 @Component({
   components: {
     AppCreditCardForm,
+    AppCreditCardList,
   },
   computed: mapGetters([Getters.selectedProducts]),
 })
