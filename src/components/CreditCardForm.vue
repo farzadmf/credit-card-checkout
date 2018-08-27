86c628426f860fcf5633b285cd75dee029e71b38
Farzad Majidfayyaz <farzadmf@hotmail.com>
2018-08-27 00:08:27 -0400

    Start the credit card form

---
+++ src/components/CreditCardForm.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+  v-card
+    v-card-title.headline New Credit Card
+    v-card-text
+      v-form
+        v-layout(row wrap)
+          v-flex(sm6 pr-2)
+            v-text-field(
+              label="Name"
+              required
+            )
+          v-flex(sm6 pl-2)
+            v-text-field(
+              label="Email"
+              required
+            )
+    v-card-actions
+      v-flex(xs-12 text-xs-center)
+        v-btn(block color="green" @click="close") Close
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class AppCreditCardForm extends Vue {
+  private close() {
+    this.$emit('close');
+  }
+}
+</script>
